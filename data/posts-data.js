af77697270bb4d3892276497d8deb9b236a0c862
gousili0916 <coolgsl@qq.com>
2017-12-24 20:24:31 +0800

    列表页

---
+++ data/posts-data.js
@@ -0,0 +1,68 @@
+var local_database =
+  [
+    {
+      "date": "Sep 18 2017",
+      "author": "大诗人",
+      "title": "致橡树",
+      "imgSrc": "/images/post/sls.jpg",
+      "content": "我如果爱你，绝不像攀岩的凌霄花，借你的高枝炫耀自己。我如果爱你，绝不学痴情的鸟儿，为绿叶重复单调的歌曲。",
+      "reading": 112,
+      "collection": 88,
+      "avatar": "/images/avatar/1.png"
+    },
+    {
+      "date": "Nov 28 2017",
+      "author": "大魔王",
+      "title": "铂金和王者的差距在哪里？",
+      "imgSrc": "/images/post/wz.png",
+      "content": "一个信号就能叫齐足够多队友,各个位置分工明确,极少死亡,这是王者局常有的事 ...王者荣耀:王者荣耀越来越受欢迎,lol会被农药超越吗?",
+      "reading": 324,
+      "collection": 222,
+      "avatar": "/images/avatar/2.png"
+    },
+    {
+      "date": "Nov 29 2017",
+      "author": "爆肝玩家zz",
+      "title": "那些年让你恨之入骨的怪物，复活币收费站",
+      "imgSrc": "/images/post/lr.png",
+      "content": "dnf前期版本，副本是很难刷的，30+的人来带僵尸，烈焰格拉卡都是很正常的。几乎每个地图都会有人带，下面让我们来回忆一下，有哪些怪物让你一定交了复活币",
+      "reading": 168,
+      "collection": 124,
+      "avatar": "/images/avatar/3.png"
+    },
+    {
+      //按住alt + shift + F 可以格式化代码样式
+      title: "当我们在谈论经济学时，我们在谈论什么?",
+      content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+      imgSrc: "/images/post/sls.jpg",
+      reading: 62,
+      collection: 92,
+      author: "知乎",
+      date: "Nov 12 2016",
+      avatar: "/images/avatar/3.png",
+    },
+    {
+      title: "微信·小程序开发工具安装指南",
+      content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+      imgSrc: "/images/post/xiaolong.jpg",
+      reading: 102,
+      collection: 92,
+      author: "猫是猫的猫",
+      date: "Nov 20 2016",
+      avatar: "/images/avatar/5.png",
+    },
+    {
+      title: "从视觉到触觉 这款VR手套能给你真实触感",
+      content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+      imgSrc: "/images/post/vr.png",
+      reading: 102,
+      collection: 26,
+      author: "深白色",
+      date: "Nov 20 2016",
+      avatar: "../../../images/avatar/3.png",
+    },
+  ]
+
+  module.exports = {
+    postList:local_database
+  }
